adea9fbfa65c4d4fd031c9129a2e9d79d084dcb8
alejandroguerravillegas <coreartalex2@gmail.com>
2022-09-12 10:30:48 -0700

    Menu

--- mock-up/src/App.js
+++ mock-up/src/App.js
@@ -1,35 +1,49 @@
-import './App.css';
 import Home from '../src/components/Home'
 import QuienesSomos from '../src/components/QuienesSomos'
 import Servicios from '../src/components/Servicios'
 import Blog from '../src/components/Blog'
 import Contact from '../src/components/Contact'
 import {StyleMain} from '../src/styles/Main.styles'
-import {BrowserRouter,Router,Route,Link, Routes} from 'react-router-dom'
+import {BrowserRouter as Router,Route,Link,Routes} from 'react-router-dom'
 
 
 function App() {
   return (
     <StyleMain>
-      <div className="App">
         <Router>
-          <h3 className='title' id='title-menu'>Mock-Up</h3>
-          <image className='logo' id='logo-menu'></image>
-          <Link className='home' id='home-menu'>Home</Link>
-          <Link>Quienes Somos</Link>
-          <Link>Servicios</Link>
-          <Link>Blog</Link>
-          <Link>Contacto</Link>
+          <img src=''></img>
+          <h3>Mock-Up</h3>
+          <nav>
+            <Link 
+            to="/" 
+            className='home' 
+            id='home-menu'>Home</Link>
+            <Link 
+            to="/quienessomos"
+            className='quienessomos'
+            id='quienessomos-menu'>Quienes Somos</Link>
+            <Link 
+            to="/servicios"
+            className='servicios'
+            id='servicios-menu'>Servicios</Link>
+            <Link 
+            to="/blog"
+            className='blog'
+            id='blog-menu'>Blog</Link>
+            <Link 
+            to="contact"
+            className='contact'
+            id='contact-menu'>Contacto</Link>
+          </nav>
           <button>LogIn</button>
+          <Routes>
+            <Route path="/" element={<Home/>}/>
+            <Route path="/quienessomos" element={<QuienesSomos/>}/>
+            <Route path="/servicios" element={<Servicios/>}/>
+            <Route path="/blog" element={<Blog/>}/>
+            <Route path="/contact" element={<Contact/>}/>
+          </Routes>
         </Router>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/quienessomos' element={<QuienesSomos/>}/>
-          <Route path='/servicios' element={<Servicios/>}/>
-          <Route path='/blog' element={<Blog/>}/>
-          <Route path='/contacto' element={<Contact/>}/>
-        </Routes>
-      </div>
     </StyleMain>
   );
 }
